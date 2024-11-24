6f6ee1df2c66515f521e0c34b543f6f8252eb29d
happyhappy-jun <bjyoon513@gmail.com>
2024-11-24 11:53:09 +0900

    feat: Add initial project setup with setup.py and utility modules

---
+++ setup.py
@@ -0,0 +1,11 @@
+# my_package/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="RSP-LLM",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
