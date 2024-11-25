cd5d4cd83b024eb8503979c9c55287d11919ea9f
happyhappy-jun <bjyoon513@gmail.com>
2024-11-25 12:27:33 +0900

    refactor: Move PairedRandomResizedCrop to separate module

--- util/kinetics.py
+++ util/kinetics.py
@@ -8,47 +8,7 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 import torchvision.transforms.functional as F
-
-
-
-class PairedRandomResizedCrop:
-    def __init__(
-        self,
-        hflip_p=0.5,
-        size=(224, 224),
-        scale=(0.5, 1.0),
-        ratio=(3./4., 4./3.),
-        interpolation=F.InterpolationMode.BICUBIC
-    ):
-        self.hflip_p = hflip_p
-        self.size = size
-        self.scale = scale
-        self.ratio = ratio
-        self.interpolation = interpolation
-
-    def __call__(self, np_RGB_img_1, np_RGB_img_2):
-        # Convert numpy images to PIL Images
-        pil_RGB_img_1 = F.to_pil_image(np_RGB_img_1)
-        pil_RGB_img_2 = F.to_pil_image(np_RGB_img_2)
-
-        i, j, h, w = transforms.RandomResizedCrop.get_params(
-            pil_RGB_img_1, scale=self.scale, ratio=self.ratio
-        )
-        # Apply the crop on both images
-        cropped_img_1 = F.resized_crop(pil_RGB_img_1,
-                                       i, j, h, w,
-                                       size=self.size,
-                                       interpolation=self.interpolation)
-        cropped_img_2 = F.resized_crop(pil_RGB_img_2,
-                                       i, j, h, w,
-                                       size=self.size,
-                                       interpolation=self.interpolation)
-
-        if random.random() < self.hflip_p:
-            cropped_img_1 = F.hflip(cropped_img_1)
-            cropped_img_2 = F.hflip(cropped_img_2)
-
-        return cropped_img_1, cropped_img_2
+from util.misc import PairedRandomResizedCrop
 
 
 class PairedKinetics(Dataset):
