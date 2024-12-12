dc4f954eac64fc7380ce76aa8787aec41053c838
happyhappy <bjyoon513@gmail.com>
2024-12-12 22:17:10 +0900

    remove debug

--- modeling/models_mse.py
+++ modeling/models_mse.py
@@ -78,9 +78,7 @@ class RspCaptionMse(RspCaption):
         prior_z = prior_dist.rsample()
 
         embedding = embedding.view(-1, 1, embedding.size(-1))
-        print(embedding.shape)
         h_context = self.resize_embed(embedding, self.decoder_embed_dim)
-        print(h_context.shape)
 
         h_context = h_context + self.language_type_embed
         # Project context to prior space
