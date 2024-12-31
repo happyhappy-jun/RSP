b88b74d3c7a2eff19df4d22b440dc25ffb9595f2
happyhappy <bjyoon513@gmail.com>
2024-12-31 11:25:43 +0900

    minimize cos

--- modeling/models_cos.py
+++ modeling/models_cos.py
@@ -96,7 +96,7 @@ class RspCaptionCos(RspCaption):
         h_context_prime = h_context_prime.view(h_context.shape)  # Ensure same shape as h_context
         context_loss = 1 - torch.nn.functional.cosine_similarity(h_context.squeeze(1), h_context_prime.squeeze(1), dim=1)
         context_loss = context_loss.mean()
-        
+
         # MAE
         img_h, mask, ids_restore = self.forward_encoder(tgt_imgs, mask_ratio=self.mask_ratio)
         pred_masked = self.forward_decoder_mae(img_h, ids_restore)
