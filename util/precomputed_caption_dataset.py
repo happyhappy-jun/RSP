77f67b0cca654b19992ddbccfa9375046a514d5c
Byungjun Yoon <bjyoon513@gmail.com>
2025-02-15 19:44:10 +0900

    fix bug

--- util/precomputed_caption_dataset.py
+++ util/precomputed_caption_dataset.py
@@ -112,10 +112,6 @@ class PrecomputedCaptionDataset(Dataset):
             selected_pairs = random.sample(pool, self.repeated_sampling)
         else:
             selected_pairs = pool
-        print(f"Selected pairs for video {video_path}: {[pair['frame_indices'] for pair in selected_pairs]}")
-        print(
-            f"Loading video {video_path} with {len(pool)} pairs, selected {len(selected_pairs)} pairs for processing.")
-        exit()
         src_images = []
         tgt_images = []
         captions = []
